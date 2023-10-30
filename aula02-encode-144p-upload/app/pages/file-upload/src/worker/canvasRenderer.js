c937c85f07a87cccb2b951ad99faac42cb7061a3
jrgvf <jorge.rodrigues.gvf@gmail.com>
2023-10-29 21:02:23 -0300

    Init aula 02

---
+++ aula02-encode-144p-upload/app/pages/file-upload/src/worker/canvasRenderer.js
@@ -0,0 +1,44 @@
+/** @type {HTMLCanvasElement} _canvas */
+let _canvas
+/** @type {RenderingContext} _ctx */
+let _ctx
+
+export default class CanvasRenderer {
+    /** @param {VideoFrame} frame  */
+    static draw(frame) {
+        const { displayHeight, displayWidth } = frame
+
+        _canvas.width = displayWidth
+        _canvas.height = displayHeight
+        _ctx.drawImage(
+            frame,
+            0, 0,
+            displayWidth,
+            displayHeight
+        )
+        frame.close()
+    }
+
+    /** @param {HTMLCanvasElement} canvas */
+    static getRenderer(canvas) {
+        _canvas = canvas
+        _ctx = canvas.getContext('2d')
+
+        const renderer = this
+        let pendingFrame = null
+        return frame => {
+            const renderAnimationFrame = () => {
+                renderer.draw(pendingFrame)
+                pendingFrame = null
+            }
+
+            if (!pendingFrame) {
+                requestAnimationFrame(renderAnimationFrame)
+            } else {
+                pendingFrame.close()
+            }
+
+            pendingFrame = frame
+        }
+    }
+}
